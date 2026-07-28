07c18896552b8ae792b76689ce80d7a9ae3088ea
Juan Ávila <juan.avila17@uptc.edu.co>
2026-07-28 08:33:52 -0500

    Initial Commit

---
+++ AdultingXP/AdultingXPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdultingXPApp.swift
+//  AdultingXP
+//
+//  Created by Juan Manuel Ávila Ávila on 28/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AdultingXPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdultingXP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdultingXP
+//
+//  Created by Juan Manuel Ávila Ávila on 28/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
